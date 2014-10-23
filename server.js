c644cf37748e5c07757788494434bb88b14d36f8
pchatterton <pachatterton@gmail.com>
2014-10-23 14:23:48 -0600

    server changes

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(function (req, res, next) {
 
 var port = Number(process.env.PORT || 3000);
 
-app.all('/*', function(req, res, next) {
-	if(req.url == '/') {
-		res.sendfile('index.html', {root: __dirname + '/'});
-	} else {
-		res.sendfile('index.html', {root: __dirname + '/'});
-	}
-})
+// app.all('/*', function(req, res, next) {
+// 	if(req.url == '/') {
+// 		res.sendfile('index.html', {root: __dirname + '/'});
+// 	} else {
+// 		res.sendfile('index.html', {root: __dirname + '/'});
+// 	}
+// })
 
 app.listen(port);
